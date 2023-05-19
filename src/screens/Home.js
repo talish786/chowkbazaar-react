166811ca4d31e7178e264eb928cf86db1db6fadd
talish786 <jam.talish@gmail.com>
2023-05-20 03:25:51 +0500

    updated home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="w-full bg-indigo-600 flex justify-center items-center">
+    <div className="bg-black1 h-screen flex justify-center items-center">
         <from className="bg-black2 p-5 w-10/12 sm:w-8/12 md:w-6/12 lg:w-4/12 rounded">
-          <h1 className="text-base">Home</h1>
+          <h3 className="mb-4 text-white capitalize font-semibold text-lg">Home</h3>
+          <div className="m-4">
+          </div>
         </from>
     </div>
   )
