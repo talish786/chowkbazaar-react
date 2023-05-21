b4f955f1162a1c08115acca86ff1848c7a740be3
talish786 <jam.talish@gmail.com>
2023-05-21 14:07:12 +0500

    Home Client updated

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="bg-black1 h-screen flex justify-center items-center">
         <from className="bg-black2 p-5 w-10/12 sm:w-8/12 md:w-6/12 lg:w-4/12 rounded">
-          <h3 className="mb-4 text-white capitalize font-semibold text-lg">Home</h3>
+          <h3 className="mb-4 text-white capitalize font-semibold text-lg">Home Updated</h3>
           <div className="m-4">
           </div>
         </from>
